f84bd5a0cb82901d1f777e0b428cfef892dc7f56
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2022-08-12 08:02:58 +0200

    Extracting cell centroids

--- src/image/read_raw_2.cpp
+++ src/image/read_raw_2.cpp
@@ -4,6 +4,16 @@
 #include <opencv2/opencv.hpp>
 #include "Input_Parser.h"
 
+template <typename S>
+std::ostream& operator<<(std::ostream& os, const std::vector<S>& v)
+{
+    // Printing all the elements using <<
+    for (auto element : v) {
+        os << element << " ";
+    }
+    return os;
+}
+
 std::vector<float> read_raw(const std::string &fpath, int width, int height, int length){
     // open the file
     std::streampos fsize;
@@ -49,7 +59,6 @@ cv::Mat get_z_mean(std::vector<float> img, int w, int h, int l, int c=0, float i
                 if (pixval > img_max) pixval = img_max;
                 if (pixval < img_min) pixval = img_min;
                 // compute iterative mean for each pixel along z-axis
-                // https://www.heikohoffmann.de/htmlthesis/node134.html
                 if (z == 0)
                     img_arr[col + w * row] = pixval;
                 else
@@ -66,10 +75,17 @@ cv::Mat get_z_mean(std::vector<float> img, int w, int h, int l, int c=0, float i
 void detect_battery_rectangle(const cv::Mat& img, int &x1_, int &x2_, int &y1_, int &y2_){
 
     cv::Mat img_integral;
-    cv::integral(img, img_integral);
-    
+    cv::integral(img, img_integral, CV_32F);
+
+    int img_integral_height = img_integral.rows;
+    int img_integral_width = img_integral.cols;
+
     int img_height = img.rows;
     int img_width = img.cols;
+    
+    // float t1 = img_integral.at<float>(img_integral_height-1, img_integral_width-1);
+    // float t2 = img.at<float>(img_height-1, img_width-1);
+    // std::cout << t1 << " " << t2 << " " << img_integral_height << " " << img_integral_width << std::endl;
 
     int w_min = (int)round(img_width * 0.10);
     int w_max = (int)round(img_width * 0.95);
@@ -78,19 +94,290 @@ void detect_battery_rectangle(const cv::Mat& img, int &x1_, int &x2_, int &y1_, 
     int h_max = (int)round(img_height * 0.95);
 
     int x1_min = (int)round(img_width * 0.05);
+    int x1_max = (int)round(img_width * 0.5);
+    int x2_min = (int)round(img_width * 0.5);
+    int x2_max = (int)round(img_width * 0.95);
+
+    int y1_min = (int)round(img_height * 0.05);
+    int y1_max = (int)round(img_height * 0.5);
+    int y2_min = (int)round(img_height * 0.5);
+    int y2_max = (int)round(img_height * 0.95);
+
+    int step =1;
+
+    float score_max = -std::numeric_limits<float>::infinity();
 
     std::cout << w_min << ", " << w_max << std::endl;
 
     x1_ = -1;
     x2_ = -1;
+    // loop through the sampled rectangles
+    for (size_t x1 = x1_min; x1 < x2_max; x1+=step)
+    {
+        for (size_t x2 = x2_min; x2 < x2_max; x2+=step)
+        {
+            if (x2>=x1 && x2-x1 >= w_min && x2-x1 <= w_max) {
+                float s1 = (img_integral.at<float>(img_integral_height-1, x1) + 
+                            img_integral.at<float>(0, 0) -
+                            img_integral.at<float>(0, x1) - 
+                            img_integral.at<float>(img_integral_height-1, 0))/(float)(x1*img_height);
+                
+                float s2 = (img_integral.at<float>(img_integral_height-1, x2) + 
+                            img_integral.at<float>(0, x1) -
+                            img_integral.at<float>(0, x2) - 
+                            img_integral.at<float>(img_integral_height-1, x1))/(float)((x2-x1)*img_height);
 
-
+                float s3 = (img_integral.at<float>(img_integral_height-1, img_integral_width-1) + 
+                            img_integral.at<float>(0, x2) -
+                            img_integral.at<float>(0, img_integral_width-1) - 
+                            img_integral.at<float>(img_integral_height-1, x2))/(float)((img_width-x2)*img_height);
+                
+                float score = s2 - 0.5 * (s1 + s3);
+                
+                if (score > score_max) {
+                    score_max = score;
+                    x1_ = x1;
+                    x2_ = x2;
+                }
+            }
+        }
+    }
+    
+    score_max = -std::numeric_limits<float>::infinity();
     y1_ = -1;
     y2_ = -1;
+
+    for (size_t y1 = y1_min; y1 < y1_max; y1+=step)
+    {
+        for (size_t y2 = y2_min; y2 < y2_max; y2+=step)
+        {
+            if (y2>=y1 && y2-y1 >= h_min && y2-y1 <= h_max) {
+                float s1 = (img_integral.at<float>(y1, img_integral_width-1) + 
+                            img_integral.at<float>(0, 0) -
+                            img_integral.at<float>(y1, 0) - 
+                            img_integral.at<float>(0, img_integral_width-1))/(float)(y1*img_width);
+                
+                float s2 = (img_integral.at<float>(y2, img_integral_width-1) + 
+                            img_integral.at<float>(y1, 0) -
+                            img_integral.at<float>(y2, 0) - 
+                            img_integral.at<float>(y1, img_integral_width-1))/(float)((y2-y1)*img_width);
+                
+                float s3 = (img_integral.at<float>(img_integral_height-1, img_integral_width-1) + 
+                            img_integral.at<float>(y2, 0) -
+                            img_integral.at<float>(img_integral_height-1, 0) - 
+                            img_integral.at<float>(y2, img_integral_width-1))/(float)((img_height-y2)*img_width);
+                
+                float score = s2 - 0.5 * (s1 + s3);
+                
+                if (score > score_max) {
+                    score_max = score;
+                    y1_ = y1;
+                    y2_ = y2;
+                }
+            }
+        }
+        
+    }
+    
 }
 
-void get_cell_locations(const cv::Mat& img, int nr_cells=18, int gap_width=5) {
-    std::vector<std::vector<float>> cell_centroids;
+std::vector<int> detect_cells(cv::Mat img, int gap_width=7, int nr_cells=18) {
+    
+    std::vector<int> x_out;
+
+    if ((gap_width % 2) == 0 || (nr_cells % 2) == 1) {
+        std::cerr << "Gap width (" << gap_width << ") must be odd number and number of cells (" << nr_cells << ") must be even number" << std::endl;
+        return x_out;
+    }
+
+    cv::Mat img_integral;
+    cv::integral(img, img_integral, CV_32F);
+
+    int img_integral_height = img_integral.rows;
+    int img_integral_width = img_integral.cols;
+
+    int img_height = img.rows;
+    int img_width = img.cols;
+
+    int step = 1;
+
+    int nhood = (int)(round(img_width/nr_cells)*0.25);
+
+
+    // central division
+    float score_optim = -std::numeric_limits<float>::infinity();
+    int x_optim = -1;
+
+    // std::cout << "nhood = " << nhood << " | " << step << score_optim << std::endl;
+    for (int dx = -nhood; dx < nhood; dx+=step)
+    {
+        // std::cout << "dx=" << dx << std::endl;
+        int x0 = img_width/(int)2 + dx;
+        int x1 = x0 - gap_width/(int)2;
+        int x2 = x0 + gap_width/(int)2;
+        int x11 = x1 - gap_width;
+        int x22 = x2 + gap_width;
+
+        float s0 = (img_integral.at<float>(img_integral_height-1, x2) + 
+                    img_integral.at<float>(0, x1) -
+                    img_integral.at<float>(0, x2) - 
+                    img_integral.at<float>(img_integral_height-1, x1))/(float)(gap_width*img_height);
+
+        float s1 = (img_integral.at<float>(img_integral_height-1, x1) + 
+                    img_integral.at<float>(0, x11) -
+                    img_integral.at<float>(0, x1) - 
+                    img_integral.at<float>(img_integral_height-1, x11))/(float)(gap_width*img_height);
+        
+        float s2 = (img_integral.at<float>(img_integral_height-1, x22) + 
+                    img_integral.at<float>(0, x2) -
+                    img_integral.at<float>(0, x22) - 
+                    img_integral.at<float>(img_integral_height-1, x2))/(float)(gap_width*img_height);
+        
+        float score = 0.5*(s1+s2)-s0;
+
+        // std::cout << score << std::endl;
+        
+        if (score > score_optim){
+            score_optim = score;
+            x_optim = x0;
+        }
+    }
+
+    x_out.push_back(x_optim);
+
+    int x_left = x_optim, x_right = x_optim;
+
+    // expand from center towards right
+    for (int i = 0; i < nr_cells/(int)2; i++)
+    {
+        score_optim = -std::numeric_limits<float>::infinity();
+        x_optim = -1;
+        for (int dx = -nhood; dx < nhood; dx+=step)
+        {
+            int x0 = x_right + (int)round(img_width/nr_cells) + dx;
+            int x1 = x0 - gap_width/(int)2;
+            int x2 = x0 + gap_width/(int)2;
+            int x11 = x1 - gap_width;
+            int x22 = x2 + gap_width;
+
+            if (x22 <= img_width) {
+                float s0 = (img_integral.at<float>(img_integral_height-1, x2) + 
+                            img_integral.at<float>(0, x1) -
+                            img_integral.at<float>(0, x2) - 
+                            img_integral.at<float>(img_integral_height-1, x1))/float(gap_width*img_height);
+                
+                float s1 = (img_integral.at<float>(img_integral_height-1, x1) + 
+                            img_integral.at<float>(0, x11) -
+                            img_integral.at<float>(0, x1) - 
+                            img_integral.at<float>(img_integral_height-1, x11))/float(gap_width*img_height);
+                
+                float s2 = (img_integral.at<float>(img_integral_height-1, x22) + 
+                            img_integral.at<float>(0, x2) -
+                            img_integral.at<float>(0, x22) - 
+                            img_integral.at<float>(img_integral_height-1, x2)) /float(gap_width*img_height);
+                
+                float score = 0.5*(s1+s2)-s0;
+
+                if (score > score_optim){
+                    score_optim = score;
+                    x_optim = x0;
+                }
+                    
+            }
+        }
+
+        if (x_optim != -1)
+        {
+            x_out.push_back(x_optim);
+            x_right = x_optim;
+        }
+        else
+        {
+            break; // stop further with for-loop first time score was not found
+        }
+    }
+
+    // expand from center towards left
+    for (int i = 0; i < nr_cells/(int)2; i++){
+        score_optim = -std::numeric_limits<float>::infinity();
+        x_optim = -1;
+        for (int dx = -nhood; dx < nhood; dx+=step){
+            int x0 = x_left - (int)round(img_width/nr_cells) + dx;
+            int x1 = x0 - gap_width/(int)2;
+            int x2 = x0 + gap_width/(int)2;
+            int x11 = x1 - gap_width;
+            int x22 = x2 + gap_width;
+
+            if (x11 >= 0) {
+                float s0 = (img_integral.at<float>(img_integral_height-1, x2) + 
+                            img_integral.at<float>(0, x1) -
+                            img_integral.at<float>(0, x2) - 
+                            img_integral.at<float>(img_integral_height-1, x1))/float(gap_width*img_height);
+                
+                float s1 = (img_integral.at<float>(img_integral_height-1, x1) + 
+                            img_integral.at<float>(0, x11) -
+                            img_integral.at<float>(0, x1) - 
+                            img_integral.at<float>(img_integral_height-1, x11))/float(gap_width*img_height);
+                
+                float s2 = (img_integral.at<float>(img_integral_height-1, x22) + 
+                            img_integral.at<float>(0, x2) -
+                            img_integral.at<float>(0, x22) - 
+                            img_integral.at<float>(img_integral_height-1, x2)) /float(gap_width*img_height);
+                
+                float score = 0.5*(s1+s2)-s0;
+
+                if (score > score_optim){
+                    score_optim = score;
+                    x_optim = x0;
+                }
+            }
+        }
+        if (x_optim != -1)
+        {
+            x_out.push_back(x_optim);
+            x_left = x_optim;
+        }
+        else
+        {
+            break; // stop further with for-loop first time score was not found
+        }
+    }
+
+    sort(x_out.begin(), x_out.end());
+
+    return x_out;
+        
+}
+
+void get_cell_locations(const cv::Mat& img, std::vector<std::vector<float>> &cell_centroids, std::vector<float> &cell_widths, int nr_cells=18, int gap_width=5) {
+
+    int x1, x2, y1, y2;
+    detect_battery_rectangle(img, x1, x2, y1, y2);
+    std::cout << x1 << ", " << x2 << ", " << y1 << ", " << y2 << std::endl;
+
+    cv::Mat img_crop = img(cv::Range(y1,y2), cv::Range(x1,x2));
+
+    std::vector<int> x_vert = detect_cells(img_crop, gap_width, nr_cells);
+    std::cout << "x_vert=[" << x_vert << "]" << std::endl;
+
+    // extract cell centroids
+    for (auto i : cell_centroids)
+        i.clear();
+
+    cell_centroids.clear();
+
+    cell_widths.clear();
+    
+    for (int i = 1; i < x_vert.size(); i++)
+    {
+        int x_r = x_vert[i];
+        int x_l = x_vert[i-1];
+        std::vector<float> cc{ x1+0.5f*(x_l+x_r), 0.5f*(y1+y2) };
+        cell_centroids.push_back(cc);
+        cell_widths.push_back(x_r - x_l);
+    }
+    
+
 
 }
 
@@ -119,18 +406,24 @@ int main(int argc, char **argv)
 
             std::vector<float> img = read_raw(filename, w, h, l);
 
-            cv::Mat img_avg = get_z_mean(img, w, h, l); //cv::Mat(h, w, CV_32F);
+            cv::Mat img_avg = get_z_mean(img, w, h, l);
 
-            // int nr_cells = 18;
-            // int gap_width = 7;
-            int x1, x2, y1, y2;
-            detect_battery_rectangle(img_avg, x1, x2, y1, y2);
+            int nr_cells = 18;
+            int gap_width = 5;
 
-            std::cout << x1 << ", " << x2 << ", " << y1 << ", " << y2 << std::endl;
+            std::vector<std::vector<float>> cell_centroids;
+            std::vector<float> cell_widths;
 
-            // get_cell_locations(, );
+            get_cell_locations(img_avg, cell_centroids, cell_widths, nr_cells, gap_width);
 
-            // save
+            std::cout << "cell centroids:" << std::endl;
+            for (auto cc : cell_centroids) 
+                std::cout << "(" << cc[0] << ", " << cc[1] << "), ";
+            std::cout << std::endl;
+
+            std::cout << "cell widths: [" << cell_widths << "]" << std::endl;
+
+            // save img_avg
             std::string fpath_out=filename.substr(0,filename.find_last_of('.'))+"_meanZ.tif";
             std::vector<int> compression_params;
             compression_params.push_back(cv::IMWRITE_TIFF_COMPRESSION);
